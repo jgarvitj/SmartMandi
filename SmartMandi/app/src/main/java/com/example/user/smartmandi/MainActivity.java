724b47a06a5cedbb63191dfd99295e10620fcf94
jgarvitj <jain.garvit4@gmail.com>
2018-07-08 01:52:46 +0530

    initial_ui

--- SmartMandi/app/src/main/java/com/example/user/smartmandi/MainActivity.java
+++ SmartMandi/app/src/main/java/com/example/user/smartmandi/MainActivity.java
@@ -75,7 +75,6 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-    @Override
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -84,17 +83,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_Farmer) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_Customer) {
 
         }
 
